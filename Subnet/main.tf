7e050bbac0cf42b474b2ed70e3d8dd0ea74bdfdb
1998arunrekha <arunar16061998@gmail.com>
2022-09-22 20:26:32 +0530

    ccc

--- Subnet/main.tf
+++ Subnet/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet11
   resource_group_name  = var.resourcegroup
-  virtual_network_name = module.VirtualNetwork.vnet_name_out.name
+  virtual_network_name = module.VirtualNetwork.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
